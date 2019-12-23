efc753ebe249d80b7ed3a39d8964a31c958b840d
PoSeiDonTeaM <johnkotsas@me.com>
2019-12-23 11:31:29 +0100

    Automated scripts added

---
+++ LAB3/mcpat_power.sh
@@ -0,0 +1,93 @@
+#! /bin/bash
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip1.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip1.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip1.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip1.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip2.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip2.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip2.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip2.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip3.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip3.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip3.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip3.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip4.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip4.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip4.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip4.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip5.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip5.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip5.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip5.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip6.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip6.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip6.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip6.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip7.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip7.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip7.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip7.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip8.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip8.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip8.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip8.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip9.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip9.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip9.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip9.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip10.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip10.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip10.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip10.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip11.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip11.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip11.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip11.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip12.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip12.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip12.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip12.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip13.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip13.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip13.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip13.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip14.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip14.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip14.txt /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip14.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+cd /home/poseidon/my_gem5/my_mcpat/mcpat && ./mcpat -infile /home/poseidon/my_gem5/mcpat_speczip/speczip15.xml -print_level 5 > /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip15.txt
+
+cd /home/poseidon/my_gem5/my_mcpat/Scripts && ./print_energy.py /home/poseidon/my_gem5/mcpat_speczip/txt_results/speczip15.txt /home/poseidon/my_gem5/speczip_results/specbzip15/stats.txt > /home/poseidon/my_gem5/mcpat_speczip/txt_results/energy/speczip15.txt
+
+aplay /home/poseidon/my_gem5/notification.wav
+
+
---
+++ LAB3/script_mcpat.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip1/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip1.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip2/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip2.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip3/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip3.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip4/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip4.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip5/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip5.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip6/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip6.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip7/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip7.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip8/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip8.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip9/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip9.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip10/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip10.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip11/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip11.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip12/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip12.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip13/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip13.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip14/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip14.xml
+
+./GEM5ToMcPAT.py /home/poseidon/my_gem5/speczip_results/specbzip1/stats.txt /home/poseidon/my_gem5/speczip_results/specbzip15/config.json /home/poseidon/my_gem5/my_mcpat/mcpat/ProcessorDescriptionFiles/inorder_arm.xml -o /home/poseidon/my_gem5/mcpat_speczip/speczip15.xml
+
